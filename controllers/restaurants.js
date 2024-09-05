07fc4768f772ca56dca8350aca2cd3e1ef768709
Faris <farissharayri@gmail.com>
2024-09-05 15:09:17 -0400

    implements index function

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -5,7 +5,13 @@ import { v2 as cloudinary } from 'cloudinary'
 
 
 async function index(req, res) {
-  
+  try {
+    const restaurants = await Restaurant.find({}).populate(['dishes', 'owner']).sort({createdAt:'desc'})
+    res.status(200).json(restaurants)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 }
 
 export {
