5fef922e2490d23e618a5cf70201aa149aebe77a
Faris <farissharayri@gmail.com>
2024-09-06 13:33:25 -0400

    implements create dish functionality

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import './config/database.js'
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as restaurantRouter } from './routes/restaurant.js'
+import { router as dishesRouter } from './routes/dishes.js'
 
 // create the express app
 const app = express()
@@ -26,6 +27,7 @@ app.use(formData.parse())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/restaurants', restaurantRouter)
+app.use('/api/dishes', dishesRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
