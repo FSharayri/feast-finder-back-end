9b5801570ac0b68554b645501754b43bf525e8c9
Faris <farissharayri@gmail.com>
2024-09-06 15:29:10 -0400

    adds review delete functionality

--- routes/dishes.js
+++ routes/dishes.js
@@ -13,16 +13,21 @@ router.get('/:dishId', dishesCtrl.show)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
+//dishes CUD
 // POST api/dishes
 router.post('/', checkAuth, dishesCtrl.create)
 // PUT api/dishes/:dishId
 router.put('/:dishId', checkAuth, dishesCtrl.update)
 // DELETE api/dishes/:dishId
 router.delete('/:dishId', checkAuth, dishesCtrl.delete)
+
+//reviews CUD
 // POST localhost:3001/api/restaurants/:dishId/reviews
 router.post('/:dishId/reviews', checkAuth, dishesCtrl.createReview)
-// PUT api/dishes/:dishId/reviews
+// PUT api/dishes/:dishId/reviews/:reviewId
 router.put('/:dishId/reviews/:reviewId', checkAuth, dishesCtrl.updateReview)
+// DELETE /api/dishes/:dishId/reviews/:reviewId
+router.delete('/:dishId/reviews/:reviewId', checkAuth, dishesCtrl.deleteReview)
 
 
 
